985bab92c3e44ec9cfdd70492c79e2044d141935
Giacomo Baso <gbaso@users.noreply.github.com>
2026-01-16 17:05:16 +0100

    add application

---
+++ src/main/java/io/github/gbaso/springsessioncookie/SpringSessionCookieApplication.java
@@ -0,0 +1,13 @@
+package io.github.gbaso.springsessioncookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionCookieApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSessionCookieApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/github/gbaso/springsessioncookie/SpringSessionCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gbaso.springsessioncookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionCookieApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
